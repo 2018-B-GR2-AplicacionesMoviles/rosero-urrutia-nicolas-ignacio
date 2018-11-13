e365d5c70e859e5195cf035d6603115e81b9cc74
NicoRosero <nicolas.nacho93@gmail.com>
2018-11-13 17:05:53 -0500

    pp1

---
+++ 2018-Moviles-Proyecto02Botones/feature/src/main/java/com/example/nirou/a2018_moviles_proyecto02botones/feature/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nirou.a2018_moviles_proyecto02botones.feature
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
