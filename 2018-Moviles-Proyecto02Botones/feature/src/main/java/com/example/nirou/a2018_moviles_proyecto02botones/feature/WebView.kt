909895506d77b562bdc17cb2798f166bc490a95c
NirouNacho <nicolas.nacho93@gmail.com>
2018-11-23 14:21:24 -0500

    11
    qw

--- 2018-Moviles-Proyecto02Botones/feature/src/main/java/com/example/nirou/a2018_moviles_proyecto02botones/feature/WebView.kt
+++ 2018-Moviles-Proyecto02Botones/feature/src/main/java/com/example/nirou/a2018_moviles_proyecto02botones/feature/WebView.kt
@@ -3,24 +3,27 @@ package com.example.nirou.a2018_moviles_proyecto02botones.feature
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebViewClient
+import android.webkit.WebView
 import kotlinx.android.synthetic.main.activity_web_view.*
 
 class WebView : AppCompatActivity() {
+
+
+    var mywebview: WebView?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
 
-       /* var webViewClient= HelloWebViewClient()
+        mywebview = findViewById<WebView>(R.id.webView)
 
-        mWebView.webViewClient = HelloWebViewClient()
-        WebView.setWebContentsDebuggingEnabled(false)
-
-
-        webView.settings.javaScriptEnabled=true
-
-        webView.webViewClient=  webViewClient
-        webView!!.loadUrl("https://www.youtube.com/watch?v=lbvV3Zeb6lo")*/
+        mywebview!!.webViewClient= object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+        mywebview!!.loadUrl("https://www.google.com.ec/")
     }
 
 
